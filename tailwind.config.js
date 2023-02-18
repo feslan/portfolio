8267ca0d2ef25f93268c5c32412c3df779191bf2
ferhat-aslan <aslanferhat16@gmail.com>
2023-02-18 21:47:02 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // content: ["./**/*.html"],
+  content: ["*.html", "./index.html"],
+  important: true,
+
+  theme: {
+    extend: {
+      colors: {
+        "daily-dev-tips": "#F89283",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
